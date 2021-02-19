7ee29f3ee9db8194f15a7bb968ba998edb644dff
Shayan Nathan <shy@Shayans-MacBook-Pro.local>
2021-02-18 16:36:04 -0800

    Login and Sign Up work

--- passport-frontend/src/Routes.js
+++ passport-frontend/src/Routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 
 export default function Routes() {
@@ -8,6 +10,12 @@ export default function Routes() {
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path="/login">
+        <Login />
       </Route>
       {/* Finally, catch all unmatched routes */}
       <Route>
--- passport-frontend/src/config.js
+++ passport-frontend/src/config.js
@@ -1,19 +1,18 @@
 const config = {
-    MAX_ATTACHMENT_SIZE: 5000000,
-    s3: {
-      REGION: "us-east-2",
-      BUCKET: "notes-app-upload-shayan",
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://uk8zl19qge.execute-api.us-east-1.amazonaws.com/prod",
-    },
-    cognito: {
-      REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_9nKwZ5eBC",
-      APP_CLIENT_ID: "3bgt063hf9mcs6smriaepsfo4k",
-      IDENTITY_POOL_ID: "us-east-2:bd7abe7b-0998-40de-8fc9-e56405c3cae5",
-    },
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "passport-app-upload",
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://2y2ztkkbjk.execute-api.us-west-2.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_2LUzwluEH",
+    APP_CLIENT_ID: "1c948fblu6cfsii9o1ebb5b5n2",
+    IDENTITY_POOL_ID: "us-east-2:ff9b2353-b947-4060-b711-c82d1b9c1054",
+  },
 };
-  
+
 export default config;
